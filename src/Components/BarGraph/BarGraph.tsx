cf59cd420e94e16056fe0145036d2a19efd3e6e7
pankajsahhu <127102412+pankajsahhu@users.noreply.github.com>
2023-04-16 22:46:45 +0530

    bar

--- src/Components/BarGraph/BarGraph.tsx
+++ src/Components/BarGraph/BarGraph.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import './BarGraph'
+import "./BarGraph";
 import * as echarts from "echarts";
 import data from "../../asset/Wine-Data.json";
 
@@ -11,19 +11,32 @@ const BarGraph: React.FC = () => {
       const chart = echarts.init(chartRef.current);
       const option = {
         xAxis: {
-          name: "Flavanoids",
+          name: "Alcohol",
           type: "value",
+          axisLabel: {
+            axisLabel: {
+              margin: 50, // add extra margin to the bottom of the label
+            },
+          },
+         
+          min: 0.5,
+          max: 4,
         },
         yAxis: {
-          name: "Ash",
+          name: "Magnesium",
           type: "value",
+          axisLabel: {
+            axisLabel: {
+              margin: 100, // add extra margin to the bottom of the label
+              offset: 0, // add extra offset to move the label to the right
+            },
+          },
         },
         series: [
           {
             type: "bar",
-            data: data.map((item, id) => {
-              return [item.Flavanoids, item.Ash];
-            }),
+            data: data.map((item) => [item.Alcohol, item.Magnesium]),
+            barMaxWidth: 30,
           },
         ],
       };
